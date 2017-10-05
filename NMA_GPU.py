bf50d82d3a4c6a70a6c7d96a63bf8aa4d52c1798
Chengchanghang <31535625+Chengchanghang@users.noreply.github.com>
2017-10-05 11:57:31 +0800

    两个隐藏层，在输入的文件进行了一次shuffle

--- NMA_GPU.py
+++ NMA_GPU.py
@@ -1,3 +1,4 @@
+
 import torch
 import torch.nn as nn
 import torchvision.datasets as dsets
@@ -6,15 +7,16 @@ from torch.autograd import Variable
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
+from matplotlib.pyplot  import *
 
 # Hyper Parameters 
 input_size = 1201
 hidden_size1 = 1000
 hidden_size2 = 1000
 num_classes = 51
-num_epochs =1 
+num_epochs = 50
 batch_size = 50
-learning_rate = 0.01
+learning_rate = 0.0001
 
 def ver(y):
     z = np.zeros((10,1))
@@ -24,20 +26,24 @@ def ver(y):
 #data loader
 IR = np.load('../NMA_data/IR_spectra_G_15.npy')[:10000]
 Structure = np.load('../NMA_data/structs.npy')[:10000]
-training_x = torch.FloatTensor(IR[:9000])
-test_x = torch.FloatTensor(IR[9000:])
-training_y = Structure[:9000]
-test_y = Structure[9000:]
-training_data = []
-test_data = []
-for _ in range(len(training_x)):
-    training_data.append((training_x[_],training_y[_]))
-for _ in range(len(test_x)):
-    test_data.append((test_x[_],test_y[_]))
+IR = np.load('/home/chengch/NMA_data/IR_spectra_G_15.npy')[:10000]
+Structure = np.load('/home/chengch/NMA_data/structs.npy')[:10000]
 
-train_dataset = training_data
-test_dataset = test_data
+total_data = []
+for x,y in zip(IR,Structure):
+    total_data.append((x,y))
+np.random.shuffle(total_data)
 
+train_data = total_data[:9000]
+test_data = total_data[9000:]
+training_data =[]
+testing_data = []
+
+for x1,y1 in train_data:
+    training_data.append((torch.FloatTensor(x1),y1)) 
+for x2,y2 in test_data:
+    testing_data.append((torch.FloatTensor(x2),y2))
+    
 # Data Loader (Input Pipeline)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
                                            batch_size=batch_size, 
@@ -69,42 +75,31 @@ net = net.cuda()
 # Loss and Optimizer
 criterion = nn.MSELoss()  
 optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)  
+plt.figure(figsize=(9,7))
 y_set = []
 # Train the Model
 start = time()
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader): 
-        # Convert torch tensor to Variable
         images = Variable(images).cuda()
-       
         labels = Variable(torch.FloatTensor(labels.numpy())).cuda()
-     
-        # Forward + Backward + Optimize
         optimizer.zero_grad()  # zero the gradient buffer
         outputs = net(images)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        y_set.append(loss.data[0])
-       
-        if (i+1) % 10 == 0:
-            print ('Epoch [%d/%d], Step [%d/%d], Loss: %.4f' %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
-           # y_set.append(loss.data[0])
-stop = time()
-print (str(stop -start))
-'''
-# Test the Model
-correct = 0
-total = 0
-for images, labels in test_loader:
-    images = Variable(images.view(-1, 28*28)).cuda()
-    outputs = net(images)
-    _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    correct += (predicted.cpu() == labels).sum()
-
-print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
-
-# Save the Model
-torch.save(net.state_dict(), 'model.pkl')
-'''
+    y_set.append(loss.data[0])
+    print (loss.data[0])
+x_set = [x for x in range(len(y_set))]
+plt.plot(x_set,y_set)
+#savefig('100.png',dpi=200)
+plt.show()
+#test_data
+loss_set = []
+for _,(images,labels) in enumerate(test_loader):
+    images = Variable(images).cuda()
+    labels = Variable(torch.FloatTensor(labels.numpy())).cuda()
+    prediction = net(images)
+    loss = criterion(prediction,labels)
+    loss_set.append(loss.data[0])
+print (np.mean(loss_set))
